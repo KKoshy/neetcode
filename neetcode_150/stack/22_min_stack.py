de1464fb777bbb7ea42acc561d6b50cf3930f945
KKoshy <40890763+KKoshy@users.noreply.github.com>
2026-08-24 21:29:48 +0530

    Create 22_min_stack.py

---
+++ neetcode_150/stack/22_min_stack.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = None
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min is None:
+            self.min = val
+        else:
+            self.min = min(self.min, val)
+        
+    def pop(self) -> None:
+        self.stack.pop()
+        if self.stack:
+            self.min = min(self.stack)
+        else:
+            self.min = None
+        
+    def top(self) -> int:
+        return self.stack[-1]
+        
+    def getMin(self) -> int:
+        return self.min
+        
